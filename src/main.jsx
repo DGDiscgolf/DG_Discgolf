69cc5649bec201fa9d55d7f08e3e66c7afa986f3
DGDiscgolf <d.gagne@outlook.com>
2026-03-18 18:30:29 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import PrivacyPage from "./pages/PrivacyPage.jsx";
+import ContactPage from "./pages/ContactPage.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/confidentialite" element={<PrivacyPage />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
