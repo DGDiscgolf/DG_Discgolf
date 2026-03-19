ce959e4ddbde323eadc4bdbfb4caddff2976e63a
DGDiscgolf <d.gagne@outlook.com>
2026-03-18 20:48:04 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import App from "./App.jsx";
 import PrivacyPage from "./pages/PrivacyPage.jsx";
 import ContactPage from "./pages/ContactPage.jsx";
 import PackagesPage from "./pages/PackagesPage.jsx";
+import CliniquePage from "./pages/CliniquePage.jsx";
+import FormationPage from "./pages/FormationPage.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -15,6 +17,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="/confidentialite" element={<PrivacyPage />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/forfaits" element={<PackagesPage />} />
+        <Route path="/clinique" element={<CliniquePage />} />
+        <Route path="/formation" element={<FormationPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
